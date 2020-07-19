c6a395e4bec8ecc42b9e1d7ea21e73ca7c940ce0
jbayensu <65293887+jbayensu@users.noreply.github.com>
2020-07-19 18:39:39 +0000

    Update test.py
    added division function

--- test.py
+++ test.py
@@ -10,6 +10,13 @@ def mult(num1, num2):
     return num1 * num2
 
 
+def div(num1, num2):
+    if(num1 > num2):
+        print("your first value should be greater")
+        return 0
+    return num2 / num1
+
+
 sum = add(4, 7)
 print(sum)
 
@@ -18,3 +25,9 @@ print(diff)
 
 product = mult(5, 3)
 print(product)
+
+quotient = div(3 > 15)
+print(quotient)
+
+quotient = div(15/3)
+print(quotient)

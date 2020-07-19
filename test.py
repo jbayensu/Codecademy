3c6b9722342aee5fc95515607f28c62d96659ae4
jbayensu <65293887+jbayensu@users.noreply.github.com>
2020-07-19 18:29:31 +0000

    Update test.py
    added subtraction function

--- test.py
+++ test.py
@@ -2,12 +2,19 @@ def add(num1, num2):
     return num1 + num2
 
 
+def sub(num1, num2):
+    return num1 - num2
+
+
 def mult(num1, num2):
     return num1 * num2
 
 
 sum = add(4, 7)
 print(sum)
+
+diff = sub(27, 13)
+print(diff)
 
 product = mult(5, 3)
 print(product)

c5d0b0ce8c361dd55b07c51bc6c3b6421455e27f
jbayensu <65293887+jbayensu@users.noreply.github.com>
2020-07-22 18:59:41 +0000

    Added function to generate message for traveler and present attractions they might be interested in.

--- the_boredless_tourist.py
+++ the_boredless_tourist.py
@@ -25,7 +25,7 @@ def add_attraction(destination, attraction):
         return
 
 
-def find_attraction(destination, interests):
+def find_attractions(destination, interests):
     destination_index = get_destination_index(destination)
     attractions_in_city = attractions[destination_index]
     attractions_with_interest = []
@@ -37,6 +37,22 @@ def find_attraction(destination, interests):
                 attractions_with_interest.append(possible_attraction[0])
 
     return attractions_with_interest
+
+
+def get_attractions_for_traveler(traveler):
+    traveler_destination = traveler[1]
+    traveler_interests = traveler[2]
+    traveler_attractions = find_attractions(
+        traveler_destination, traveler_interests)
+    interests_string = "Hi " + \
+        traveler[0] + ", we think you'll like these places around " + \
+        traveler_destination + ": "
+    for i in range(len(traveler_attractions)):
+        if traveler_attractions[-1] == traveler_attractions[i]:
+            interests_string += " the " + traveler_attractions[i] + "."
+        else:
+            interests_string += " the " + traveler_attractions[i] + ", "
+    return interests_string
 
 
 # print(get_destination_index("Los Angeles, USA"))
@@ -63,5 +79,9 @@ add_attraction("Cairo, Egypt", ["Pyramids of Giza", [
                "monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
 
-la_arts = find_attraction("Los Angeles, USA", ['art'])
+la_arts = find_attractions("Los Angeles, USA", ['art'])
 print(la_arts)
+
+smills_france = get_attractions_for_traveler(
+    ['Dereck Smill', 'Paris, France', ['monument']])
+print(smills_france)

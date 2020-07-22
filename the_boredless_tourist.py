11ab5c3446f268084e5c22906e16792949035c8b
jbayensu <65293887+jbayensu@users.noreply.github.com>
2020-07-22 18:40:30 +0000

    Added interest finder logic

--- the_boredless_tourist.py
+++ the_boredless_tourist.py
@@ -25,16 +25,29 @@ def add_attraction(destination, attraction):
         return
 
 
-print(get_destination_index("Los Angeles, USA"))
-print(get_destination_index("Paris, France"))
-#print(get_destination_index("Hyderabad, India"))
+def find_attraction(destination, interests):
+    destination_index = get_destination_index(destination)
+    attractions_in_city = attractions[destination_index]
+    attractions_with_interest = []
+    for attraction in attractions_in_city:
+        possible_attraction = attraction
+        attraction_tags = attraction[1]
+        for interest in interests:
+            if(interest in attraction_tags):
+                attractions_with_interest.append(possible_attraction[0])
+
+    return attractions_with_interest
+
+
+# print(get_destination_index("Los Angeles, USA"))
+# print(get_destination_index("Paris, France"))
+# print(get_destination_index("Hyderabad, India"))
 
 test_destination_index = get_traveler_location(test_traveler)
-print(test_destination_index)
+# print(test_destination_index)
 
-print(attractions)
+# adding attraction
 add_attraction("Los Angeles, USA", ['Venice Beach', ['beach']])
-print(attractions)
 add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
 add_attraction("Paris, France", ["Arc de Triomphe", [
                "historical site", "monument"]])
@@ -49,3 +62,6 @@ add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]]
 add_attraction("Cairo, Egypt", ["Pyramids of Giza", [
                "monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+
+la_arts = find_attraction("Los Angeles, USA", ['art'])
+print(la_arts)
